2a5251031d327fd402ca794f3f1e8c2b8ce80f51
MembriniLuca29 <lucamembrini29@gmail.com>
2024-01-24 02:29:01 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/Homepage.vue'
+
+function searchLocation() {
+
+    let locationName = document.getElementById("locationInput").value;
+
+  
+    let resultContainer = document.getElementById("resultContainer");
+    resultContainer.innerHTML = "<h3>Risultati della ricerca:</h3><p>" + locationName + "</p>";
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+<header>
+  <div><img src="../public/Cattura.PNG" class="logo" alt=""></div>
+  <div class="search-container">
+        <input type="text" id="locationInput" placeholder="Inserisci il nome della località">
+        <button onclick="searchLocation()">Cerca</button>
+    </div>
+  <div class="link"><a href="">Registrati</a><a href="">Accedi</a></div>
+</header>
+  <Homepage/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+header{
+  height: 15vh;
+  background-color: rgb(192, 168, 168);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.logo{
+  height: 15vh;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.link{
+  display: flex;
+  align-items: center;
+  justify-content: space-around; 
+  width: 20%;
+}
+header>.link>a{
+  font-size: 2rem;
+  text-decoration: none;
+  margin: 0 20px;
+  color: black;
+}
+header>.link>a:hover{
+  font-size: 2.2rem;
+  text-decoration: underline;
+  color: blue;
+}
+.search-container {
+    display: flex;
+    padding: 2% 0;
+}
+
+input {
+    padding: 10px;
+    font-size: 16px;
+    width: 250px;
+}
+
+button {
+    padding: 10px;
+    font-size: 16px;
+    cursor: pointer;
 }
 </style>
